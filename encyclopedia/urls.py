7c73f79ed2a500e292f26ad2ada3362e27cefc9a
csabsen <csabanagy@gmx.com>
2020-12-29 19:42:28 +0100

    fixed weird wiki/HTML error, added / to each URL

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
-    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("wiki/<str:entry>/", views.entry, name="entry"),
     path("new/", views.new, name="new"),
-    path("edit/<str:page>", views.edit, name="edit"),  
+    path("edit/<str:page>/", views.edit, name="edit"),  
     path("random_page/", views.random_page, name="random")
 ]
